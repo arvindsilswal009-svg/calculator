8ab7b63b7d274ef8c8e8d01f418ebf2b670339e1
arvindsilswal009-svg <arvindsilswal009@gmail.com>
2025-11-09 14:06:32 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+ const [data,setData]=useState("");
+  const getValue=(event)=>{
+   
+    console.log(event.target.value);
+    setData(data.concat(event.target.value));
+  }
+  const calculation = () =>{
+    setData(eval(data).toString())
+
+  }
+  const back=()=>{
+    setData(data.slice(0,-1))
+  }
+  const clear=()=>{
+    setData("")
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className='container'>
+      <div className='calculator'>
+        
+          <div >
+            <input className='display' type='text' readOnly value={data} />
+          </div>
+          <div className='button '>
+            <button onClick={clear}> AC</button>
+            <button onClick={back} > DEL</button>
+            <button onClick={getValue} value="."> .</button>
+            <button onClick={getValue} value="/"> /</button>
+            <br />
+            <button onClick={getValue} value="7"> 7</button>
+            <button onClick={getValue} value="8"> 8</button>
+            <button onClick={getValue} value="9"> 9</button>
+            <button onClick={getValue} value="*"> *</button>
+            <br /> 
+            <button onClick={getValue} value="4"> 4</button>
+            <button onClick={getValue} value="5"> 5</button>
+            <button onClick={getValue} value="6"> 6</button>
+            <button onClick={getValue} value="+"> +</button>
+            <br /> 
+            <button onClick={getValue} value="1"> 1</button>
+            <button onClick={getValue} value="2"> 2</button>
+            <button onClick={getValue} value="3"> 3</button>
+            <button onClick={getValue} value="-"> -</button>
+            <br />
+            <button onClick={getValue} value="0"> 0</button>
+            <button onClick={getValue} value="00"> 00</button>
+            <button onClick={calculation} className='equal'  >=</button>
+
+          </div>
+        
+      </div>
     </div>
   );
 }
